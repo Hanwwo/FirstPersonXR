604f4c057110627d26b8efe22dee43e5ae61de7d
Hanwwo <gp18999@gmail.com>
2026-06-23 19:47:08 +0900

    초기 세팅

---
+++ Source/FirstPersonXR/FirstPersonXRGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FirstPersonXRGameMode.h"
+
+AFirstPersonXRGameMode::AFirstPersonXRGameMode()
+{
+	// stub
+}
---
+++ Source/FirstPersonXR/FirstPersonXRGameMode.h
@@ -0,0 +1,22 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FirstPersonXRGameMode.generated.h"
+
+/**
+ *  Simple GameMode for a first person game
+ */
+UCLASS(abstract)
+class AFirstPersonXRGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AFirstPersonXRGameMode();
+};
+
+
+
